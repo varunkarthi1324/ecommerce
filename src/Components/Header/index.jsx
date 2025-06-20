1bd764d20fe261e11b7ea3d6e6ddb03f00bc8ee8
varun karthikeyan M <vkvarunkarthikeyan@gmail.com>
2025-06-21 01:06:08 +0530

    v1

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <div className="top-strip">
+          <div className="container">
+            <div className="flex-items-center justify-between">
+              <div className="col1 w-[50%]">
+                <p className="text-[14px]">
+                  Get upto 50% off new season styles ,limited time only
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
